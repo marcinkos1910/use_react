c213c14076897056ae5aada818db76885bf21d83
Marcin Kos <marcin.kos.krk@gmail.com>
2022-05-12 19:43:58 +0200

    basic example

---
+++ src/State.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function StateComponent() {
+    const [isGreen, setIsGreen] = useState(() => true));
+
+    return (
+        <h1 style={{color: isGreen ? 'green' : 'blue'}} onClick={() => {
+            setIsGreen((prevState) => !prevState)
+        }}>useState example</h1>
+    )
+}
+
+export default StateComponent;
